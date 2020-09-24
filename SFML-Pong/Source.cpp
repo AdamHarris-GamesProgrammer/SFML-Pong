e6ae023751764aad7e34c8c7bef51517c0625a5a
Adam Harris <adam_harris@hotmail.com>
2020-09-24 17:24:15 +0100

    0x02:  Paddles can now move and added delta time system

--- SFML-Pong/Source.cpp
+++ SFML-Pong/Source.cpp
@@ -21,12 +21,27 @@ public:
 
 	}
 
+	void SetKeys(sf::Keyboard::Key up, sf::Keyboard::Key down) {
+		upKey = up;
+		downKey = down;
+	}
+
 	void Draw() {
 		mWindow->draw(paddleSprite);
 	}
 
-	void Update() {
+	void PollInput(float deltaTime, sf::Event* event) {
+		if (sf::Keyboard::isKeyPressed(upKey)) {
+			position.y--;
+		}
+		else if (sf::Keyboard::isKeyPressed(downKey)) {
+			position.y++;
+		}
+	}
 
+	void Update(float deltaTime, sf::Event* event) {
+		PollInput(deltaTime, event);
+		paddleSprite.setPosition(position);
 	}
 
 private:
@@ -36,35 +51,39 @@ private:
 
 	sf::RenderWindow* mWindow;
 
+	sf::Keyboard::Key upKey;
+	sf::Keyboard::Key downKey;
 };
 
 
 int main()
 {
-	sf::RenderWindow* window = new sf::RenderWindow(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT, 32), "SFML Pong");
-	
+	auto* window = new sf::RenderWindow(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "SFML Pong");
+	window->setVerticalSyncEnabled(true);
+	window->setFramerateLimit(60);
+
 	Paddle leftPaddle(window, 15, sf::Color::Red, sf::Vector2f(20,100));
+	leftPaddle.SetKeys(sf::Keyboard::W, sf::Keyboard::S);
+
 	Paddle rightPaddle(window, SCREEN_WIDTH - 35, sf::Color::Green, sf::Vector2f(20,100));
+	rightPaddle.SetKeys(sf::Keyboard::Up, sf::Keyboard::Down);
+
+	sf::Clock deltaClock;
 
 	while (window->isOpen())
 	{
+		sf::Time dt = deltaClock.restart();
+
 		sf::Event event;
 		while (window->pollEvent(event))
 		{
-
-
 			if (event.type == sf::Event::Closed)
 				window->close();
 
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
-				//position.y--;
-			}
-			else if(sf::Keyboard::isKeyPressed(sf::Keyboard::S))
-			{
-				//position.y++;
-			}
 		}
-
+		
+		leftPaddle.Update(dt.asSeconds(), &event);
+		rightPaddle.Update(dt.asSeconds(), &event);
 
 		window->clear();
 
