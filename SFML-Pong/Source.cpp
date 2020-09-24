c135a22c0874e2eb6beff3afaf69ccc2c9d9e257
Adam Harris <adam_harris@hotmail.com>
2020-09-24 16:44:31 +0100

    0x01: Created a basic Paddle class

--- SFML-Pong/Source.cpp
+++ SFML-Pong/Source.cpp
@@ -1,23 +1,78 @@
 #include <SFML/Graphics.hpp>
+#include <memory>
+
+#define SCREEN_WIDTH 1024
+#define SCREEN_HEIGHT 720
+
+void SetPaddlePosition(sf::RectangleShape& paddle, sf::Vector2f pos) {
+	paddle.setPosition(pos);
+}
+
+class Paddle {
+public:
+	Paddle() = default;
+	Paddle(sf::RenderWindow* window, float xPos, sf::Color color, sf::Vector2f size) : mWindow(window), color(color), position(xPos, SCREEN_HEIGHT / 2)
+	{
+		paddleSprite.setSize(size);
+		paddleSprite.setPosition(position);
+		paddleSprite.setFillColor(color);
+	}
+	~Paddle() {
+
+	}
+
+	void Draw() {
+		mWindow->draw(paddleSprite);
+	}
+
+	void Update() {
+
+	}
+
+private:
+	sf::RectangleShape paddleSprite;
+	sf::Vector2f position;
+	sf::Color color;
+
+	sf::RenderWindow* mWindow;
+
+};
+
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+	sf::RenderWindow* window = new sf::RenderWindow(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT, 32), "SFML Pong");
+	
+	Paddle leftPaddle(window, 15, sf::Color::Red, sf::Vector2f(20,100));
+	Paddle rightPaddle(window, SCREEN_WIDTH - 35, sf::Color::Green, sf::Vector2f(20,100));
 
-	while (window.isOpen())
+	while (window->isOpen())
 	{
 		sf::Event event;
-		while (window.pollEvent(event))
+		while (window->pollEvent(event))
 		{
+
+
 			if (event.type == sf::Event::Closed)
-				window.close();
+				window->close();
+
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
+				//position.y--;
+			}
+			else if(sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+			{
+				//position.y++;
+			}
 		}
 
-		window.clear();
-		window.draw(shape);
-		window.display();
+
+		window->clear();
+
+		leftPaddle.Draw();
+		rightPaddle.Draw();
+
+
+		window->display();
 	}
 
 	return 0;
