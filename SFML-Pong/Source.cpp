6b55b004129bc9a62d2884af3f4759b50c5c6ef3
Adam Harris <adam_harris@hotmail.com>
2020-09-27 21:46:53 +0100

    0x07: Added a SoundEffect class to simplify creating a sound effect

--- SFML-Pong/Source.cpp
+++ SFML-Pong/Source.cpp
@@ -8,6 +8,47 @@
 #define SCREEN_WIDTH 1024
 #define SCREEN_HEIGHT 720
 
+class SoundEffect {
+public:
+	SoundEffect() = default;
+
+	SoundEffect(std::string& filename) {
+		SetPath(filename);
+	}
+
+	void SetPath(std::string& filename) {
+		if (!soundBuffer.loadFromFile(filename)) {
+			std::cout << "[ERROR: Sound.cpp]: Sound buffer at " << filename << " could not be found" << std::endl;
+			return;
+		}
+		soundEffect.setBuffer(soundBuffer);
+	}
+
+	void SetPath(const char* filename) {
+		if (!soundBuffer.loadFromFile(filename)) {
+			std::cout << "[ERROR: Sound.cpp]: Sound buffer at " << filename << " could not be found" << std::endl;
+			return;
+		}
+		soundEffect.setBuffer(soundBuffer);
+	}
+	void Play() {
+		soundEffect.play();
+	}
+	void Pause() {
+		soundEffect.pause();
+	}
+	void Stop() {
+		soundEffect.stop();
+	}
+	void SetLooping(bool isLooped) {
+		soundEffect.setLoop(isLooped);
+	}
+
+private:
+	sf::SoundBuffer soundBuffer;
+	sf::Sound soundEffect;
+};
+
 class GameObject {
 public:
 	GameObject(sf::RenderWindow* window, sf::Vector2f position, sf::Color colorr, sf::Vector2f size) : window(window), position(position), color(color), size(size) {
@@ -62,11 +103,11 @@ public:
 
 	void PollInput(float deltaTime, sf::Event* event) override {
 		if (sf::Keyboard::isKeyPressed(upKey)) {
-			if(position.y > 0)
+			if (position.y > 0)
 				position.y = position.y - movementSpeed * deltaTime;
 		}
 		else if (sf::Keyboard::isKeyPressed(downKey)) {
-			if(position.y < SCREEN_HEIGHT - size.y)
+			if (position.y < SCREEN_HEIGHT - size.y)
 				position.y = position.y + movementSpeed * deltaTime;
 		}
 	}
@@ -77,7 +118,7 @@ public:
 		PollInput(deltaTime, event);
 		paddleSprite.setPosition(position);
 
-		
+
 	}
 
 private:
@@ -91,19 +132,12 @@ private:
 
 class Ball : public GameObject {
 public:
-	Ball(sf::RenderWindow* window, sf::Color color, float radius) : GameObject(window, sf::Vector2f(SCREEN_WIDTH/2,SCREEN_HEIGHT/2), color, sf::Vector2f(radius * 2, radius * 2)) {
+	Ball(sf::RenderWindow* window, sf::Color color, float radius) : GameObject(window, sf::Vector2f(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2), color, sf::Vector2f(radius * 2, radius * 2)) {
 		ballSprite = sf::CircleShape(radius);
 		Reset();
 
-		if (!scoreSoundBuffer.loadFromFile("Assets/Sounds/paddleHit.Wav")) {
-			std::cout << "Failed to load file" << std::endl;
-		}
-		scoreSound.setBuffer(scoreSoundBuffer);
-
-		if (!wallSoundBuffer.loadFromFile("Assets/Sounds/wallHit.wav")) {
-			std::cout << "Failed to load file" << std::endl;
-		}
-		wallSound.setBuffer(wallSoundBuffer);
+		scoreSound.SetPath("Assets/Sounds/paddleHit.Wav");
+		wallSound.SetPath("Assets/Sounds/wallHit.wav");
 	}
 
 	void Draw() override {
@@ -136,15 +170,15 @@ public:
 		position = sf::Vector2f(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
 
 		//generates random seed for the program
-		std::random_device rd;  
-		std::mt19937 gen(rd()); 
+		std::random_device rd;
+		std::mt19937 gen(rd());
 		std::uniform_int_distribution<> dis(0, 1);
 		std::uniform_real_distribution<> yDis(0, movementSpeed);
 
 		float xVel = movementSpeed * dis(gen);
 		float yVel = movementSpeed * dis(gen);
 
-		if(dis(gen) == 0)
+		if (dis(gen) == 0)
 		{
 			xVel = -movementSpeed;
 		}
@@ -155,7 +189,7 @@ public:
 
 		if (dis(gen) == 0)
 		{
-			
+
 			yVel = -yDis(gen);
 
 		}
@@ -168,8 +202,7 @@ public:
 	}
 
 	void HitPaddle() {
-		//TODO: Play sound
-		scoreSound.play();
+		scoreSound.Play();
 
 		if (position.x < SCREEN_WIDTH / 2) {
 			position.x = 0 + 36.0f;
@@ -202,7 +235,7 @@ public:
 
 	void HitWall() {
 		velocity.y = -velocity.y;
-		wallSound.play();
+		wallSound.Play();
 	}
 
 private:
@@ -212,10 +245,8 @@ private:
 
 	float movementSpeed = 250.0f;
 
-	sf::SoundBuffer scoreSoundBuffer;
-	sf::SoundBuffer wallSoundBuffer;
-	sf::Sound scoreSound;
-	sf::Sound wallSound;
+	SoundEffect scoreSound;
+	SoundEffect wallSound;
 };
 
 bool CheckCollision(GameObject& a, GameObject& b) {
@@ -231,10 +262,10 @@ int main()
 	window->setVerticalSyncEnabled(true);
 	window->setFramerateLimit(60);
 
-	std::shared_ptr<Paddle> leftPaddle = std::make_shared<Paddle>(window, 15, sf::Color::Red, sf::Vector2f(20,100), 120.0f);
+	std::shared_ptr<Paddle> leftPaddle = std::make_shared<Paddle>(window, 15, sf::Color::Red, sf::Vector2f(20, 100), 120.0f);
 	leftPaddle->SetKeys(sf::Keyboard::W, sf::Keyboard::S);
 
-	std::shared_ptr<Paddle> rightPaddle = std::make_shared<Paddle>(window, SCREEN_WIDTH - 35, sf::Color::Green, sf::Vector2f(20,100), 120.0f);
+	std::shared_ptr<Paddle> rightPaddle = std::make_shared<Paddle>(window, SCREEN_WIDTH - 35, sf::Color::Green, sf::Vector2f(20, 100), 120.0f);
 
 	std::shared_ptr<Ball> ball = std::make_shared<Ball>(window, sf::Color::White, 16.0f);
 
@@ -281,7 +312,7 @@ int main()
 
 		window->display();
 	}
-	
+
 	delete window;
 
 	return 0;
